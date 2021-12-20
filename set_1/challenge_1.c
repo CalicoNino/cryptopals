dbc4ccd640be4080f0b94de26f30f1f35e84b563
elshenak <elshenak@gmail.com>
2021-12-19 21:44:08 -0500

    Challenge 3 and 4 start

--- set_1/challenge_1.c
+++ set_1/challenge_1.c
@@ -11,50 +11,58 @@ const char *output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2
 // Global Bytes to Base64
 char *byte_to_base64[65] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"};
 
-unsigned int *str_to_hexbytes(const char *hex_str) {
+unsigned int *str_to_hexbytes(const char *hex_str)
+{
     size_t len = strlen(hex_str);
-    unsigned int *hex = malloc(sizeof(unsigned int)* len/2);
-    for(int i = 0, j = 0; i < len; i += 2, j++) { 
+    unsigned int *hex = malloc(sizeof(unsigned int) * len / 2);
+    for (int i = 0, j = 0; i < len; i += 2, j++)
+    {
         char tmp[3];
-        strncpy(tmp, hex_str+i, 2);
-        tmp[2]=0;
+        strncpy(tmp, hex_str + i, 2);
+        tmp[2] = 0;
         hex[j] = strtol(tmp, NULL, 16);
     }
     return hex;
 }
 
-unsigned int *hexbytes_to_base64bytes(unsigned int *hexbytes, size_t len) {
+unsigned int *hexbytes_to_base64bytes(unsigned int *hexbytes, size_t len)
+{
     unsigned int *base64bytes = malloc(sizeof(unsigned int) * len);
-    for(int i = 0; i < len; i += 3) {
+    for (int i = 0; i < len; i += 3)
+    {
         base64bytes[i] = (hexbytes[i] >> 2) & 0x3f;
-        base64bytes[i+1] = ((hexbytes[i] & 0x3) << 4) | ((hexbytes[i+1] & 0xF0) >> 4);
-        base64bytes[i+2] = ((hexbytes[i+1] & 0xf) << 2) | ((hexbytes[i+2] & 0xC0) >> 6);
-        base64bytes[i+3] = hexbytes[i+2] & 0x3f;
+        base64bytes[i + 1] = ((hexbytes[i] & 0x3) << 4) | ((hexbytes[i + 1] & 0xF0) >> 4);
+        base64bytes[i + 2] = ((hexbytes[i + 1] & 0xf) << 2) | ((hexbytes[i + 2] & 0xC0) >> 6);
+        base64bytes[i + 3] = hexbytes[i + 2] & 0x3f;
     }
     free(hexbytes);
     return base64bytes;
 }
 
-char *base64_to_str(unsigned int *base64bytes, size_t len) {
-    char *str = malloc(sizeof(char)*len);
-    for(int i = 0; i <= len; i++) {
+char *base64_to_str(unsigned int *base64bytes, size_t len)
+{
+    char *str = malloc(sizeof(char) * len);
+    for (int i = 0; i <= len; i++)
+    {
         strcat(str, byte_to_base64[(int)base64bytes[i]]);
     }
     free(base64bytes);
     return str;
 }
 
-char *hex_to_base64(const char *input) {
-    size_t len = strlen(input)/2;
+char *hex_to_base64(const char *input)
+{
+    size_t len = strlen(input) / 2;
     unsigned int *hex = str_to_hexbytes(input);
     unsigned int *base64 = hexbytes_to_base64bytes(hex, len);
     return base64_to_str(base64, len);
 }
 
-int main() {
+int main()
+{
 
     char *result = hex_to_base64(input);
     printf("%s\n", result);
-    
+
     return 0;
 }
--- set_1/challenge_2.c
+++ set_1/challenge_2.c
@@ -3,26 +3,30 @@
 #include <string.h>
 
 // Fixed XOR: https://cryptopals.com/sets/1/challenges/2
-// Write a function that takes two equal-length buffers and produces their XOR combination. 
+// Write a function that takes two equal-length buffers and produces their XOR combination.
 const char *input_1 = "1c0111001f010100061a024b53535009181c";
 const char *input_2 = "686974207468652062756c6c277320657965";
 const char *output = "746865206b696420646f6e277420706c6179";
 
-unsigned int *str_to_hexbytes(const char *hex_str) {
+unsigned int *str_to_hexbytes(const char *hex_str)
+{
     size_t len = strlen(hex_str);
-    unsigned int *hex = malloc(sizeof(unsigned int)* len/2);
-    for(int i = 0, j = 0; i < len; i += 2, j++) { 
+    unsigned int *hex = malloc(sizeof(unsigned int) * len / 2);
+    for (int i = 0, j = 0; i < len; i += 2, j++)
+    {
         char tmp[3];
-        strncpy(tmp, hex_str+i, 2);
-        tmp[2]=0;
+        strncpy(tmp, hex_str + i, 2);
+        tmp[2] = 0;
         hex[j] = strtol(tmp, NULL, 16);
     }
     return hex;
 }
 
-char *bytes_to_str(unsigned int *bytes, size_t len) {
-    char *str = malloc(sizeof(char)*len);
-    for(int i = 0; i < len/2; i++) {
+char *bytes_to_str(unsigned int *bytes, size_t len)
+{
+    char *str = malloc(sizeof(char) * len);
+    for (int i = 0; i < len / 2; i++)
+    {
         char tmp[3];
         sprintf(tmp, "%x", bytes[i]);
         tmp[2] = 0;
@@ -32,9 +36,11 @@ char *bytes_to_str(unsigned int *bytes, size_t len) {
     return str;
 }
 
-unsigned int *xor_hexbytes(unsigned int *hexbytes_1, unsigned int *hexbytes_2, size_t len) {
+unsigned int *xor_hexbytes(unsigned int *hexbytes_1, unsigned int *hexbytes_2, size_t len)
+{
     unsigned int *hex = malloc(sizeof(unsigned int) * len);
-    for(int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
+    {
         hex[i] = hexbytes_1[i] ^ hexbytes_2[i];
     }
     free(hexbytes_1);
@@ -42,18 +48,21 @@ unsigned int *xor_hexbytes(unsigned int *hexbytes_1, unsigned int *hexbytes_2, s
     return hex;
 }
 
-char *xor_hex(const char *input_1, const char *input_2) {
+char *xor_hex(const char *input_1, const char *input_2)
+{
     size_t len = strlen(input_1);
-    if (len == strlen(input_2)) {
+    if (len == strlen(input_2))
+    {
         unsigned int *hexbytes_1 = str_to_hexbytes(input_1);
         unsigned int *hexbytes_2 = str_to_hexbytes(input_2);
-        unsigned int *hexbytes = xor_hexbytes(hexbytes_1, hexbytes_2, len/2);
-        return bytes_to_str(hexbytes, len); 
+        unsigned int *hexbytes = xor_hexbytes(hexbytes_1, hexbytes_2, len / 2);
+        return bytes_to_str(hexbytes, len);
     }
     return 0;
 }
 
-int main(void) {
+int main(void)
+{
 
     char *result = xor_hex(input_1, input_2);
     printf("Output: %s\n", result);
